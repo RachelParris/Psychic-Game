a4f7465f74dae456f566ec9b0d489632c29b7244
rachelparris <sonnet22@gmail.com>
2017-08-22 01:33:38 -0400

    Ables to capture user input. Created functions to increment wins and decrement losses.

---
+++ assets/js/game.js
@@ -0,0 +1,40 @@
+var computerChoice = ["A", "B", "C", "D", "E", "F", "G"];
+var x = 12;
+var wins = 1;
+var losses = 0;
+
+// For each wrong guess the remaining # of guesses decrements by 1 from 12 to 0.
+// It stops at 0 and counts as 1 loss.
+function wrongAnswer() {
+  if (x > 0) {
+    x--;
+    userLost();
+  }
+}
+
+function userLost() {
+  if ( x === 0) {
+    losses++;
+    document.getElementById('lost').innerHTML = losses;
+  }
+}
+
+document.onkeyup = function(event) {
+  var input = event.key;
+  var userGuess = input.toUpperCase();
+
+  var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+  document.getElementById('computerPick').innerHTML = computerGuess;
+
+  if (userGuess === computerGuess) {
+    document.getElementById('win').innerHTML = wins++;
+    console.log("true");
+  } else {
+    document.getElementById('remainingGuesses').innerHTML = x;
+    console.log("false");
+    wrongAnswer();
+  }
+
+  document.getElementById('printGuesses').innerHTML = userGuess;
+
+}
