97df32f774f12f9e2c4a1324f47a0134a997cb62
rachelparris <sonnet22@gmail.com>
2017-08-26 07:12:15 -0400

    latest verison

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,11 @@
-var computerChoice = ["A", "B", "C", "D", "E", "F", "G"];
+var computerChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var x = 12;
-var wins = 1;
+var wins = 0;
 var losses = 0;
 
-var letters = [];
+
+document.getElementById('win').innerHTML = wins;
+document.getElementById('lost').innerHTML = losses;
 // For each wrong guess the remaining # of guesses decrements by 1 from 12 to 0.
 // It stops at 0 and counts as 1 loss.
 function wrongAnswer() {
@@ -28,7 +30,8 @@ document.onkeyup = function(event) {
   document.getElementById('computerPick').innerHTML = computerGuess;
 
   if (userGuess === computerGuess) {
-    document.getElementById('win').innerHTML = wins++;
+  	wins++
+    document.getElementById('win').innerHTML = wins;
     console.log("true");
   } else {
     document.getElementById('remainingGuesses').innerHTML = x;
@@ -36,7 +39,6 @@ document.onkeyup = function(event) {
     wrongAnswer();
   }
 
-  var addLetters = letters.push(userGuess);
   document.getElementById('printGuesses').innerHTML = userGuess;
 
 }
